1af59b44b98996fb8fe81d6a866c70b6e808688d
ramenjuniti <ramenjuniti@gmail.com>
2021-04-28 16:19:11 +0900

    サンプルコード追加

---
+++ KadaiApp2/KadaiApp2/BannerViewController.h
@@ -0,0 +1,12 @@
+//
+//  BannerViewController.h
+//  KadaiApp2
+//
+//  Created by j-kugizaki on 2021/04/28.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BannerViewController : UIViewController
+
+@end
